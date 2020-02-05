0812facc311f947abc429407c31d21089b91e313
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-05 18:23:25 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
-  validates :name, length: { minimum: 4 }
+  validates :phone_number, length: { minimum: 10 }
 end
