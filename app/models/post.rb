b58fb6390608058ee2dbc21cb5e3dc3143febd82
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-05 19:05:37 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category." }
 
-  validates :title, inclusion: { in: ["You Won't", "Believe", "Secret", "Top", "Guess"],
+  validates :title, exclusion: { in: ["You Won't", "Believe", "Secret", "Top", "Guess"],
     message: "That is not a clickbait-y title. Try again."}
 
 end
