962c34cd572dd49a0fd353679101d583a58744c5
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-05 18:51:58 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
+  validates :content, presence: true
+  validates :summary, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
 
@@ -8,5 +10,5 @@ class Post < ActiveRecord::Base
 
   validates :title, inclusion: { in: %w(Won't Believe Secret Top Guess),
     message: "%{value} is not a valid title."}
-     
+
 end
